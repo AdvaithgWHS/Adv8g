392cf237f201d4ee3c102a6ea07ed17f9e71b1f9
AdvaithgWHS <AdvaithgWHS@users.noreply.github.com>
2020-08-18 15:28:36 -0500

    interface changes

--- COE/modals.js
+++ COE/modals.js
@@ -4,6 +4,7 @@ var modal2 = document.getElementById("myModal2");
 var modal3 = document.getElementById("myModal3");
 var modal4 = document.getElementById("myModal4");
 var modal5 = document.getElementById("myModal5");
+var modal6 = document.getElementById("myModal6");
 
 // Get the button that opens the modal
 var btn = document.getElementById("infoBtn");
@@ -11,6 +12,7 @@ var btn2 = document.getElementById("myBtn2");
 var btn3 = document.getElementById("myBtn3");
 var btn4 = document.getElementById("myBtn4");
 var btn5 = document.getElementById("myBtn5");
+var btn6 = document.getElementById("myBtn6");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -18,6 +20,7 @@ var span2 = document.getElementsByClassName("close2")[0];
 var span3 = document.getElementsByClassName("close3")[0];
 var span4 = document.getElementsByClassName("close4")[0];
 var span5 = document.getElementsByClassName("close5")[0];
+var span6 = document.getElementsByClassName("close6")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
@@ -40,6 +43,10 @@ btn5.onclick = function() {
   modal5.style.display = "block";
 }
 
+btn6.onclick = function() {
+  modal6.style.display = "block";
+}
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
@@ -59,6 +66,10 @@ span4.onclick = function() {
 
 span5.onclick = function() {
   modal5.style.display = "none";
+}
+
+span6.onclick = function() {
+  modal6.style.display = "none";
 }
 
 
